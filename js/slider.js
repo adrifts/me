92e251a7e7db8d33704b042f33d6e5c504bcecda
adrifts <jessica.v.lima@gmail.com>
2016-12-25 02:38:36 +0000

    adrifts

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+$(".title").click(function(e){
+		e.preventDefault();
+        $(".imgshow").slideToggle();
+    });
+
+$(".title_2").click(function(e){
+		e.preventDefault();
+        $(".imgshow_2").slideToggle();
+    });
+$(".title_3").on('click',function(e){
+		e.preventDefault();
+        $(".imgshow_3").slideToggle();
+    });
+$(".title_4").click(function(e){
+		e.preventDefault();
+        $(".imgshow_4").slideToggle();
+    });
+$(".title_5").click(function(e){
+		e.preventDefault();
+        $(".imgshow_5").slideToggle();
+    });
